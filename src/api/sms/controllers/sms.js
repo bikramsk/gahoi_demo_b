bd2e626d043251c55448cfb812d65e231e3f42fa
bikramsk <bikramkanyal@gmail.com>
2025-06-13 12:40:55 +0530

    update sms

--- src/api/sms/controllers/sms.js
+++ src/api/sms/controllers/sms.js
@@ -1,32 +1,38 @@
 'use strict';
 
-module.exports = ({strapi}) => ({
+module.exports = ({ strapi }) => ({
   async send(ctx) {
     try {
-      const { message, number } = ctx.request.body;
+      let { message, number } = ctx.request.body;
+
+     
+      let cleanedNumber = number.toString().replace(/\D/g, '');
+
+   
+      if (cleanedNumber.length > 10) {
+        cleanedNumber = cleanedNumber.slice(-10);
+      }
 
       const formData = new URLSearchParams();
-     formData.append('api_key', process.env.SMS_API_KEY);
+      formData.append('api_key', process.env.SMS_API_KEY);
       formData.append('message', message);
-      formData.append('number', number);
+      formData.append('number', cleanedNumber); 
       formData.append('route', '1');
 
       const response = await fetch('https://www.wpsenders.in/api/sendMessage', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
+          'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: formData
+        body: formData,
       });
 
       const data = await response.json();
-      
+
       ctx.body = data;
     } catch (error) {
       ctx.body = { error: error.message };
       ctx.status = 500;
     }
-  }
+  },
 });
-
-  // formData.append('api_key', process.env.SMS_API_KEY);
