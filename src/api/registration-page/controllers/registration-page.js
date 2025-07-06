0d6687bf2064ea1c57800b7d3f0753a9f9b475c6
bikramsk <bikramkanyal@gmail.com>
2025-07-06 16:54:54 +0530

    updated controller

--- src/api/registration-page/controllers/registration-page.js
+++ src/api/registration-page/controllers/registration-page.js
@@ -1,3 +1,14 @@
+// 'use strict';
+
+// /**
+//  * registration-page controller
+//  */
+
+// const { createCoreController } = require('@strapi/strapi').factories;
+
+// module.exports = createCoreController('api::registration-page.registration-page');
+
+
 'use strict';
 
 /**
@@ -6,4 +17,22 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::registration-page.registration-page');
+module.exports = createCoreController('api::registration-page.registration-page', ({ strapi }) => ({
+  async create(ctx) {
+    // Call the default create logic
+    const response = await super.create(ctx);
+
+    // Get the mobile number from the request body
+    const mobileNumber = ctx.request.body.data?.mobileNumber;
+
+    if (mobileNumber) {
+      // Set isRegistered: true for this user
+      await strapi.query('api::mobile-user.mobile-user').update({
+        where: { mobileNumber },
+        data: { isRegistered: true }
+      });
+    }
+
+    return response;
+  }
+}));
