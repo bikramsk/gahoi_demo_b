ee32dabdecdcad5bc87c81ab6b9bf82d2c7873de
bikramsk <bikramkanyal@gmail.com>
2025-07-11 18:48:09 +0530

    added family sibling relation

--- src/api/registration-page/routes/registration-page.js
+++ src/api/registration-page/routes/registration-page.js
@@ -6,4 +6,17 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::registration-page.registration-page');
+module.exports = {
+  routes: [
+    {
+      method: 'GET',
+      path: '/registration-pages/check-family-member/:mobileNumber',
+      handler: 'registration-page.checkFamilyMember',
+      config: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+    },
+  ],
+};
